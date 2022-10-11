3eee537d0c1f9e12875eb09c1441506d3bbca8a0
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-11 10:25:17 -0400

    Added cloudinary routes and controller for multiple image uploads. Added the ability to save a product to the mongodb from the admin dashboard

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -5,6 +5,8 @@ import { useSelector } from "react-redux";
 import { createProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
+import FileUpload from "../../../components/forms/FileUpload";
+
 
 const initialState = {
     title: "Macbook Pro",
@@ -26,6 +28,7 @@ const ProductCreate = () => {
     const [values, setValues] = useState(initialState);
     const [subOptions, setSubOptions] = useState([]);
     const [showSub, setShowSub] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     // redux
     const { user } = useSelector((state) => ({ ...state }));
@@ -78,6 +81,17 @@ const ProductCreate = () => {
                 <div className="col-md-10">
                     <h4>Product create</h4>
                     <hr />
+
+                    {JSON.stringify(values.images)}
+
+
+                    <div className="p-3">
+                        <FileUpload
+                            values={values}
+                            setValues={setValues}
+                            setLoading={setLoading}
+                        />
+                    </div>
 
                     <ProductCreateForm
                         handleSubmit={handleSubmit}
